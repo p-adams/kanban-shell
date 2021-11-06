08dd98f10c0740e1a4a886de1d102ef6a13f3c42
Paul Adams <ghusiyy@gmail.com>
2021-11-05 21:29:43 -0700

    init kanban board function

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "Hello, World!"
+let init_kanban_board n =
+    let kanban_board = Hashtbl.create n
+    Hashtbl.add kanban_board "Ice Box" []
+    Hashtbl.add kanban_board "In Progress" []
+    Hashtbl.add kanban_board "Done" []
+    kanban_board
+
+
+let () =
+    let board = init_kanban_board 3
+    Hashtbl.iter (fun key val -> Printf.printf "%s\n" key) board
+    
