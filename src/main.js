7db15b8ca492dce91d8b9f6ce88ca33820399266
Paul Adams <ghusiyy@gmail.com>
2021-11-07 23:40:56 -0800

    add welcome screen

--- src/main.js
+++ src/main.js
@@ -1,30 +1,26 @@
 import { KanbanBoard } from "./modules/KanbanBoard.js";
 
-function main() {
-  // const kanbanBoard = new KanbanBoard();
-
-  const input = document.querySelector(".input-prompt");
-  const menu = document.querySelector(".menu");
-  // menu.style.visibility = "visible";
-  input.addEventListener("change", (e) => {
-    const prompt = e.target.value;
-    // remove focus from prompt, toggle menu visibility, and render menu
-    if (prompt.toUpperCase() === "MENU") {
-      menu.style.visibility = "visible";
-      input.blur();
-      menuItems.focus();
-    } else {
-      menu.style.visibility = "hidden";
-    }
-  });
-  const menuItems = document.querySelector(".menu ul");
-  menuItems.addEventListener("keydown", (e) => {
-    if (e.which === 37) {
-      console.log("nav left");
-    } else if (e.which === 39) {
-      console.log("nav right");
-    }
-  });
-}
-
-export default main;
+// const kanbanBoard = new KanbanBoard();
+const input = document.querySelector(".input-prompt");
+console.log(input);
+const menu = document.querySelector(".menu");
+// menu.style.visibility = "visible";
+input.addEventListener("change", (e) => {
+  const prompt = e.target.value;
+  // remove focus from prompt, toggle menu visibility, and render menu
+  if (prompt.toUpperCase() === "MENU") {
+    menu.style.visibility = "visible";
+    input.blur();
+    menuItems.focus();
+  } else {
+    menu.style.visibility = "hidden";
+  }
+});
+const menuItems = document.querySelector(".menu ul");
+menuItems.addEventListener("keydown", (e) => {
+  if (e.which === 37) {
+    console.log("nav left");
+  } else if (e.which === 39) {
+    console.log("nav right");
+  }
+});
