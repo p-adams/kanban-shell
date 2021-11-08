d339ddc65a5a057d52aa3bb0eac5085f6e49b7f3
Paul Adams <ghusiyy@gmail.com>
2021-11-07 19:27:44 -0800

    add keydown event on menu nav

--- src/main.js
+++ src/main.js
@@ -5,13 +5,24 @@ function main() {
 
   const input = document.querySelector(".input-prompt");
   const menu = document.querySelector(".menu");
+  // menu.style.visibility = "visible";
   input.addEventListener("change", (e) => {
     const prompt = e.target.value;
-    // toggle menu visibility and render menu
+    // remove focus from prompt, toggle menu visibility, and render menu
     if (prompt.toUpperCase() === "MENU") {
       menu.style.visibility = "visible";
+      input.blur();
+      menuItems.focus();
     } else {
       menu.style.visibility = "hidden";
+    }
+  });
+  const menuItems = document.querySelector(".menu ul");
+  menuItems.addEventListener("keydown", (e) => {
+    if (e.which === 37) {
+      console.log("nav left");
+    } else if (e.which === 39) {
+      console.log("nav right");
     }
   });
 }
