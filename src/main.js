f6f8d3503f25d28fb061fb3f0107d088f2c2dc7e
Paul Adams <ghusiyy@gmail.com>
2021-11-08 23:51:50 -0800

    refactor callback to async/await

--- src/main.js
+++ src/main.js
@@ -2,18 +2,17 @@ import { KanbanBoard } from "./modules/KanbanBoard.js";
 import { handleInput } from "./modules/Input.js";
 import { handleMenuNavigation } from "./modules/Menu.js";
 
-function main() {
+async function main() {
   const input = document.querySelector(".input-prompt");
   const menu = document.querySelector(".menu");
   const menuItems = document.querySelector(".menu ul");
   const homeScreen = document.querySelector(".menu #home-screen");
   const boardsScreen = document.querySelector(".menu #boards-screen");
   const helpScreen = document.querySelector(".menu #help-screen");
-  handleInput(input, menu, menuItems, (prompt) => {
-    if (prompt) {
-      handleMenuNavigation(menuItems, [homeScreen, boardsScreen, helpScreen]);
-    }
-  });
+  const prompt = await handleInput(input, menu, menuItems);
+  if (prompt) {
+    handleMenuNavigation(menuItems, [homeScreen, boardsScreen, helpScreen]);
+  }
 }
 
 export default main;
--- src/modules/Input.js
+++ src/modules/Input.js
@@ -1,17 +1,19 @@
-export function handleInput(input, menu, menuItems, cb) {
-  input.value = "";
-  input.addEventListener("change", (e) => {
-    const prompt = e.target.value;
-    switch (prompt.toUpperCase()) {
-      case "MENU":
-        menu.style.visibility = "visible";
-        input.blur();
-        menuItems.focus();
-        break;
-      default:
-        menu.style.visibility = "hidden";
-        break;
-    }
-    cb(prompt);
+export function handleInput(input, menu, menuItems) {
+  return new Promise((resolve, reject) => {
+    input.value = "";
+    input.addEventListener("change", (e) => {
+      const prompt = e.target.value;
+      switch (prompt.toUpperCase()) {
+        case "MENU":
+          menu.style.visibility = "visible";
+          input.blur();
+          menuItems.focus();
+          break;
+        default:
+          menu.style.visibility = "hidden";
+          break;
+      }
+      resolve(prompt);
+    });
   });
 }
